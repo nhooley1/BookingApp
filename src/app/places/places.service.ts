93d312cc75180e72de7c1525a18f84cd38943e46
Nicholas Hooley <nicholashooley27@yahoo.co.uk>
2020-06-28 20:41:00 +0100

    discover page with list of places, service created

---
+++ src/app/places/places.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { Place } from './place.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PlacesService {
+  private _places: Place[] =  [
+    
+new Place(
+  'p1',
+  'Manhattan Skyline',
+  'NY Landscape', 
+  'https://upload.wikimedia.org/wikipedia/commons/b/b9/Above_Gotham.jpg',
+  149.99
+  ),
+  new Place(
+    'p2',
+    'London Bridge',
+    'A Pretty Bridge',
+    'https://media.gettyimages.com/photos/london-uk-tower-bridge-at-river-thames-sunset-twilight-scene-picture-id155278925?s=612x612',
+    199.99
+  ),
+
+new Place(
+  'p3',
+  'Belgrade Fortress',
+  'A Cool Fortress',
+  'https://st2.depositphotos.com/2810819/10354/i/950/depositphotos_103541386-stock-photo-zindan-gate-inside-belgrade-fortress.jpg',
+  179.99
+
+)
+];
+    
+  get places() {
+
+    return [...this._places];
+
+  }
+
+  constructor() { }
+}
